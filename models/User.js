9052c36645a3988d0799e5b3f9a8672552306f3e
Crystal Aguilar <crystal_0688@yahoo.com>
2022-11-17 19:59:17 -0800

    user

--- models/User.js
+++ models/User.js
@@ -1,1 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        email: {
+            type: DataTypes.STRING
+        },
+        password: {
+            type: DataTypes.ALPHANUMERIC
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+
 module.exports = User
