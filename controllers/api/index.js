7d31f9ad499b26cf4c0e61f61c5e0f4a3e0129c1
Crystal Aguilar <crystal_0688@yahoo.com>
2022-11-17 20:29:47 -0800

    confirmation code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const projectRoutes = require('./projectRoutes');
+const serviceRoutes = require('./servicesRoutes');
 
 router.use('/users', userRoutes);
 router.use('/services', servicesRoutes);
--- models/User.js
+++ models/User.js
@@ -15,7 +15,21 @@ User.init(
         },
         password: {
             type: DataTypes.ALPHANUMERIC
-        }
+        },
+        status: {
+            type: String, 
+            enum: ['Pending', 'Active'],
+            default: 'Pending'
+          },
+          confirmationCode: { 
+            type: String, 
+            unique: true },
+          roles: [
+            {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "Role"
+            }
+          ]
     },
     {
         sequelize,
