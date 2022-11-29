108229d2a96b4bb1c7bd8783b9a92be72fcb5a7b
Crystal Aguilar <crystal_0688@yahoo.com>
2022-11-28 19:51:38 -0800

    fiename to service.json

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const userRoute = require('./userRoute');
-// const servicesRoute = require('./servicesRoute');
 
 router.use('/users', userRoute);
-// router.use('/services', servicesRoute);
 
 module.exports = router;
