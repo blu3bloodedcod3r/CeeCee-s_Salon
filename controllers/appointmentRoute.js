2c553f93afbc43d186e4728e53552b45e13329b8
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-11-29 22:42:00 -0600

    Merging

--- controllers/appointmentRoute.js
+++ controllers/appointmentRoute.js
@@ -7,34 +7,34 @@ router.get('/', async (req, res) => {
     return res.render('bookappointment')
 })
 
-var picker = function () {
-    $('#time-1').appointmentPicker({
-    interval: 60,
-    mode: '12h',
-    minTime: 09,
-    maxTime: 19,
-    startTime: 09,
-    endTime: 19,
-    disabled: ['16:30', '17:00'],
-    allowReset: true,
-    title: Services.selectService,
-    templateInner: '<li class="appo-picker-list-item {{enabled}}"><input type="button" tabindex="-1" value="{{time}}" {{enabled}}></li>',
-    templateOuter: '<span class="appo-picker-title">{{title}}</span><ul class="appo-picker-list">{{innerHtml}}</ul>',
-    }); 
-};
+    var picker = function () {
+        $('#time-1').appointmentPicker({
+        interval: 60,
+        mode: '12h',
+        minTime: 09,
+        maxTime: 19,
+        startTime: 09,
+        endTime: 19,
+        disabled: ['16:30', '17:00'],
+        allowReset: true,
+        title: Services.selectService,
+        templateInner: '<li class="appo-picker-list-item {{enabled}}"><input type="button" tabindex="-1" value="{{time}}" {{enabled}}></li>',
+        templateOuter: '<span class="appo-picker-title">{{title}}</span><ul class="appo-picker-list">{{innerHtml}}</ul>',
+        }); 
+    };
 
 AppointmentPicker.onload = function() {
-    $.fn.appointmentPicker = function(options) {
-        this.appointmentPicker = new AppointmentPicker(this[0], options);
-        return this;
-      }
-};
+        $.fn.appointmentPicker = function(options) {
+            this.appointmentPicker = new AppointmentPicker(this[0], options);
+            return this;
+        }
+    };
 
 const getTime = function() {
-    $picker.appointmentPicker.getTime();
-};
+        $picker.appointmentPicker.getTime();
+    };
 
-console.log(picker);
-console.log(getTime)
+    console.log(picker);
+    console.log(getTime)
 
 module.exports = router
