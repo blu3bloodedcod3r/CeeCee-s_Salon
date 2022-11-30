443414dc02acebb4b19f2ddd0b04e74f4c855c46
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-11-29 23:00:02 -0600

    Merge

--- controllers/appointmentRoute.js
+++ controllers/appointmentRoute.js
@@ -7,21 +7,21 @@ router.get('/', async (req, res) => {
     return res.render('bookappointment')
 })
 
-    var picker = function () {
-        $('#time-1').appointmentPicker({
-        interval: 60,
-        mode: '12h',
-        minTime: 09,
-        maxTime: 19,
-        startTime: 09,
-        endTime: 19,
-        disabled: ['16:30', '17:00'],
-        allowReset: true,
-        title: Services.selectService,
-        templateInner: '<li class="appo-picker-list-item {{enabled}}"><input type="button" tabindex="-1" value="{{time}}" {{enabled}}></li>',
-        templateOuter: '<span class="appo-picker-title">{{title}}</span><ul class="appo-picker-list">{{innerHtml}}</ul>',
-        }); 
-    };
+var picker = function () {
+    $('#time-1').appointmentPicker({
+    interval: 60,
+    mode: '12h',
+    minTime: 09,
+    maxTime: 19,
+    startTime: 09,
+    endTime: 19,
+    disabled: ['16:30', '17:00'],
+    allowReset: true,
+    title: Services.selectService,
+    templateInner: '<li class="appo-picker-list-item {{enabled}}"><input type="button" tabindex="-1" value="{{time}}" {{enabled}}></li>',
+    templateOuter: '<span class="appo-picker-title">{{title}}</span><ul class="appo-picker-list">{{innerHtml}}</ul>',
+    }); 
+};
 
 AppointmentPicker.onload = function() {
         $.fn.appointmentPicker = function(options) {
