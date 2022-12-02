3388b0bd6b934e7889dbd249a3ea1ba998616351
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-12-01 20:40:00 -0600

    Corrected book route

--- controllers/index.js
+++ controllers/index.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 
 const homeRoutes = require('./homeRoute')
 const apiRoutes = require('./api');
-// const appointmentRoute = require('./appointmentRoute')
+const bookRoute = require('./bookRoute')
 const adminRoute = require('./adminRoute');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
-// router.use('/appointment', appointmentRoute)
+router.use('/book', bookRoute)
 router.use('/admin', adminRoute);
 
 module.exports = router;
