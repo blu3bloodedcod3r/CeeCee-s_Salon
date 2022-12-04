af96cc3c292a4963cd24ff3bb4fdcbaf87404e75
carynbojames <110750060+carynbojames@users.noreply.github.com>
2022-12-03 21:35:37 -0600

    Pushing data from the form

--- public/js/appointment.js
+++ public/js/appointment.js
@@ -1,6 +1,5 @@
 // Reference: 11-Express > 17-Ins_POST-Fetch, 21_Ins_Modular-Routing
-// const AppointmentPicker = require('appointment-picker')
-// import AppointmentPicker from 'appointment-picker';
+
 
 var picker = new AppointmentPicker(document.getElementById('time-1'), {
   interval: 60,
@@ -18,25 +17,14 @@ document.getElementById('time-1').addEventListener('click', function() {
 	picker.open();
 })
 
-// document.getElementById('time-1').addEventListener('change.appo.picker', function (e) {});
-// document.getElementById('time-1').addEventListener('close.appo.picker', function (e) {});
-// document.getElementById('time-1').addEventListener('open.appo.picker', function (e) {});
 
-
-
-
-// Get the value and save it as a variable
-const email = document.getElementById("email").value.trim();
-const date = document.getElementById("date").value.trim();
-const time = document.getElementById("time-1").value.trim();
-const service = document.getElementById("description").value.trim();
 const apptForm = document.getElementById("apptForm");
 
 // Create HTML
 
 
 const postAppt = (appt) =>
-  fetch("/book/appointment", {
+  fetch("/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -44,22 +32,34 @@ const postAppt = (appt) =>
     body: JSON.stringify(appt),
   })
     .then((res) => res.json())
+    .then((data) => {return data})
     .catch(() => console.error("Error in POST request"));
+
 
 // Create the event listener. 
 apptForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  // Get the value and save it as a variable
+  // When outside fo the event listener, the variables hold no value
+  // Code was already run and didn't get value 
+  // Without value it returns the html line
+  const email = document.getElementById("email").value.trim();
+  const date = document.getElementById("date").value.trim();
+  const time = document.getElementById("time-1").value.trim();
+  const service = document.getElementById("description").value.trim();
+  const msg = document.getElementById('message').value.trim()
 
   // Create an object
   const newAppt = {
-    email: email.value.trim(),
-    date: date.value.trim(),
-    time: time.value.trim(),
-    service: service.value(),
+    email: email,
+    date: date,
+    time: time,
+    service: service,
+    message: msg
   };
+  console.log(newAppt)
 
   // Make a POST request to the server
   postAppt(newAppt)
-    .then(() => console.log("Success"))
     .catch((err) => console.error(err));
 });
